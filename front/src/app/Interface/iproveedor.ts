76d909ed54c3571097775f33aea90aa40b145124
gatos1975 <wilmerayuy@gmail.com>
2024-01-12 16:22:07 -0500

    select proveedores y stock

---
+++ front/src/app/Interface/iproveedor.ts
@@ -0,0 +1,6 @@
+export interface Iproveedor {
+  proveedorId: number;
+  nombres: string;
+  telefono: string;
+  correo: string;
+}
---
+++ front/src/app/Interface/stock.ts
@@ -0,0 +1,7 @@
+export interface Stock {
+  stockId: number;
+  productoId: number;
+  proveedorId: number;
+  cantidad: number;
+  precio_Venta: number;
+}
